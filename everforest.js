09d979672a672f3514053f377d1d7c8b11b58904
Hugo Hennies <hhennies@gmail.com>
2024-01-06 15:16:03 -0300

    create theme

---
+++ everforest.js
@@ -0,0 +1,26 @@
+black       = '#343f44';
+red         = '#e67e80';
+green       = '#a7c080';
+yellow      = '#dbbc7f';
+blue        = '#7fbbb3';
+magenta     = '#d699b6';
+cyan        = '#83c092';
+white       = '#859289';
+lightBlack  = '#3d484d';
+lightRed    = '#e67e80';
+lightGreen  = '#a7c080';
+lightYellow = '#d3c6aa';
+lightBlue   = '#7fbbb3';
+lightMagenta= '#d699b6';
+lightCyan   = '#83c092';
+lightWhite  = '#9da9a0';
+
+t.prefs_.set('color-palette-overrides',
+                 [ black , red     , green  , yellow,
+                  blue     , magenta , cyan   , white,
+                  lightBlack   , lightRed  , lightGreen , lightYellow,
+                  lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+
+t.prefs_.set('cursor-color', '#d3c6aa');
+t.prefs_.set('foreground-color', '#d3c6aa');
+t.prefs_.set('background-color', '#2d353b');
